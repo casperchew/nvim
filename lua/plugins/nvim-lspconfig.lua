ccb139ef227ac46d11145b61dcc5c747244fad29
Casper <capsulechew@gmail.com>
2025-09-25 11:21:40 +0800

    Add emmet and herb

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,8 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		vim.lsp.enable("clangd")
+		vim.lsp.enable('emmet_language_server')
+		vim.lsp.enable('herb_ls')
 		vim.lsp.enable("lua_ls")
 		vim.lsp.enable("pyright")
 		vim.lsp.enable("texlab")
