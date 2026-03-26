53fe06f5a633f540f372f119a5ca4b65c635d515
casperchew <capsulechew@gmail.com>
2026-03-26 09:03:17 +0800

    .

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
+vim.g.mapleader = "\\"
+
 require("config.lazy")
 
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+})
+
+vim.o.foldmethod = "indent"
 vim.o.list = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
